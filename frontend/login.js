de47c2207a817e5dcc49034c3d8deb85c57e4d8d
Lucaswpp <105546601+Lucaswpp@users.noreply.github.com>
2025-01-04 20:48:31 -0300

    tela de login finalizada.

--- frontend/login.js
+++ frontend/login.js
@@ -5,6 +5,36 @@ let states_page = ["login", "singup"];
 let is_login = false;
 let now_state = states_page[0];
 
+const btn_request_data = document.querySelector("#login")
+const obj_pop_up_event = document.querySelector(".pop-up-conteiner")
+const popup_btn = document.querySelector("#btn-close-popup")
+
+const input_login = document.querySelector("#input-login")
+const input_senha = document.querySelector("#input-senha")
+
+popup_btn.addEventListener("click", () => 
+    {
+        obj_pop_up_event.classList.remove("active")
+        input_login.value = ""
+        input_senha.value = ""
+    })
+
+
+btn_request_data.addEventListener("click", () => 
+    {
+        if (input_login.value != "lucas123" || input_senha.value != "123456")
+        {
+        obj_pop_up_event.classList.add("active")
+        return;
+        }
+
+        window.location.href = "./welcome.html"
+
+
+    })
+
+
+
 const dic_list_login = {
     "login": {
         ".left-title-box": "ACESSAR CONTA",
